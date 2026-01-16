c056d028d74a81f6f53dfa10151e40a2fef14be5
K4izuo <crisjustineoracion22@gmail.com>
2026-01-17 01:14:32 +0800

    edit

--- app/page/admin/asset-management/page.tsx
+++ app/page/admin/asset-management/page.tsx
@@ -15,7 +15,7 @@ export default function AssetsPage() {
   };
 
   return (
-    <div className="p-6">
+    <div className="">
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-3xl font-bold">Assets Management</h1>
         <Button
