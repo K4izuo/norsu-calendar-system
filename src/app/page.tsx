6fd6ca0d2571d5a4132c7d514c2951fd712f065f
K4izuo <crisjustineoracion22@gmail.com>
2026-02-12 18:29:48 +0800

    edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import { CalendarSkeleton } from "@/components/ui/skeleton";
 import AboutSection from "@/components/ui/about-section";
 import type { EventDetails, CalendarDayType } from "@/interface/user-props";
 import { toast } from "react-hot-toast";
-// import Link from "next/link";
+import Link from "next/link";
 import { usePublicReservations, usePublicAssets } from "@/services/reservation-service";
 import { getPhilippineDateTime, getPhilippineMonth, getPhilippineYear, getPhilippineDay } from "@/lib/timezone-utils";
 
@@ -238,13 +238,15 @@ export default function Home() {
       {/* Navbar */}
       <div className="relative bg-white px-2 sm:px-4 md:px-8 lg:px-16 xl:px-36 py-4 shadow-sm flex flex-col sm:flex-row items-center sm:items-center justify-between w-full gap-y-2">
         <div className="flex flex-row items-center justify-center sm:justify-start w-full sm:w-auto gap-2 sm:gap-0">
-          <Image
-            src="/images/norsu.png"
-            alt="Negros Oriental State University"
-            className="h-10 w-10 sm:h-12 sm:w-12 object-contain"
-            width={48}
-            height={48}
-          />
+          <Link href="/auth/sign-in">
+            <Image
+              src="/images/norsu.png"
+              alt="Negros Oriental State University"
+              className="h-10 w-10 sm:h-12 sm:w-12 object-contain cursor-pointer"
+              width={48}
+              height={48}
+            />
+          </Link>
           <div className="flex flex-col items-center min-w-0 sm:hidden ml-2">
             <h1 className="font-semibold text-xl text-gray-800 text-center truncate">
               NORSU Calendar System
